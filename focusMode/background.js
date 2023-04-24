aeab33c0f05f390e82d8cf1c65d384c878f9c260
Akash1520 <CE2020_107079@Ldceahm.gujgov.edu.in>
2023-04-24 23:32:26 +0530

    1.0.0 - NextLearn, Twitter, Extensions/webstore

--- focusMode/background.js
+++ focusMode/background.js
@@ -1,7 +1,3 @@
-const extensions = 'https://developer.chrome.com/docs/extensions'
-const webstore = 'https://developer.chrome.com/docs/webstore'
-
-
 // chrome.runtime.onInstalled.addListener(() => {
 //     chrome.action.setBadgeText({
 //         text: "OFF",
@@ -13,32 +9,54 @@ const webstore = 'https://developer.chrome.com/docs/webstore'
 
 
 chrome.action.onClicked.addListener(async (tab) => {
-  console.log(tab)
-    if (tab.url.startsWith(extensions) || tab.url.startsWith(webstore)) {
-      // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
-      const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-      // Next state will always be the opposite
-      const nextState = prevState === 'ON' ? 'OFF' : 'ON'
-  
-      // Set the action badge to the next state
-      await chrome.action.setBadgeText({
-        tabId: tab.id,
-        text: nextState,
-      });
 
-      if (nextState === "ON") {
-        // bd.style.backgroundColor="black";
-        // Insert the CSS file when the user turns the extension on
-        await chrome.scripting.insertCSS({
-          files: ["focus-mode.css"],
-          target: { tabId: tab.id },
-        });
-      } else if (nextState === "OFF") {
-        // Remove the CSS file when the user turns the extension off
-        await chrome.scripting.removeCSS({
-          files: ["focus-mode.css"],
-          target: { tabId: tab.id },
-        });
-      }
+  // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
+  const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+  // Next state will always be the opposite
+  const nextState = prevState === 'ON' ? 'OFF' : 'ON'
+
+  // Set the action badge to the next state
+  await chrome.action.setBadgeText({
+    tabId: tab.id,
+    text: nextState,
+  });
+
+  var siteArray = [
+    ["https://nextjs.org/learn","focus-nextlearn.css"],
+    ["https://developer.chrome.com/docs/webstore", "focus-mode.css"],
+    ["https://developer.chrome.com/docs/extensions", "focus-mode.css"],
+    [
+      "https://twitter.com", "focus-twitter.css"
+    ]]
+  // console.log(tab.url)
+  var cssFile = "";
+  for (let i = 0; i < siteArray.length; i++) {
+    let url = siteArray[i][0]
+    let val = siteArray[i][1]
+    console.log(tab.url.startsWith(url));
+    if(tab.url.startsWith(url)){
+      cssFile=val;
+      break;
     }
+  }
+
+
+  // console.log(cssFile);
+
+  if (nextState === "ON") {
+    // bd.style.backgroundColor="black";
+    // Insert the CSS file when the user turns the extension on
+
+    await chrome.scripting.insertCSS({
+      files: [cssFile],
+      target: { tabId: tab.id },
+    });
+
+  } else if (nextState === "OFF") {
+    // Remove the CSS file when the user turns the extension off
+    await chrome.scripting.removeCSS({
+      files: [cssFile],
+      target: { tabId: tab.id },
+    });
+  }
 })
